52483010b114841d74efc357dedef46567a9d669
Unknown <rub3n.bcn@gmail.com>
2018-10-08 13:50:51 +0200

    add plugin devtools

--- grav-admin/cache/compiled/files/6a440346c22686cb825a21c3ff4e823b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/grav-admin/user/config/security.yaml',
-    'modified' => 1538994997,
-    'data' => [
-        'salt' => 'e9RvZ7VCy8mJzt'
-    ]
-];
